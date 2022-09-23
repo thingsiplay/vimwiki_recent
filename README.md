0d7154b19f1773d9eac475e1e5afe4d5eb9ab9d5
Tuncay <redbulltrinker@googlemail.com>
2022-09-23 01:13:06 +0000

    initial upload

---
+++ README.md
@@ -0,0 +1,81 @@
+# vimwiki\_recent
+
+Read your vimwiki files sorted by last used and create a list
+
+- Author: Tuncay D.
+- Source: https://github.com/thingsiplay/vimwiki\_recent
+- License: [MIT License](LICENSE)
+
+## What is this?
+
+**vimwiki_recent** is simply a "script" to generate a list of recently used
+vimwiki files for Vim. That's the entire job of it.
+[VimWiki](https://github.com/vimwiki/vimwiki) is a plugin for
+[Vim](https://vimdoc.sourceforge.net).
+
+## How to use
+
+### Installation
+
+This script is written in Python and therefore requires Python 3 to be
+installed. Download the script, give it the executable bit and put it in a
+folder that is in the "PATH". It does not read other files than the vimwiki
+folder you point to and does not create other files than the "recent.wiki"
+file. Here is a simple instruction you can follow (you don't have to):
+
+    git clone https://github.com/thingsiplay/vimwiki_recent
+    cd vimwiki_recent
+    chdmod +x vimwiki_recent.py
+    install -m 755 vimwiki_recent.py "$(systemd-path user-binaries)/vimwiki_recent"
+
+To uninstall from that location, just delete the script or use following
+command (provided you installed it with the above command without altering it):
+
+    rm -f "$(systemd-path user-binaries)/vimwiki_recent"
+
+Notice how I removed the file extension ".py" when installing the script. The
+following examples assumes you removed the extension too, but that is
+absolutely an optional step and just my personal preference.
+
+### General functionality
+
+Just run the script from commandline with pointing to the root directory of
+your vimwiki folder. At default it will create or update a file named
+"recent.wiki" in that folder, containing a list of files as wiki links.  At the
+moment only the Vimwiki syntax language is supported. You can also output the
+content of the file that would be written to the stdout without changing the
+file. Here is an example command:
+
+    vimwiki_recent -d ~/vimwiki -Xo
+
+Omit the option `-X` and the default file will be created at
+"~/vimwiki/recent.wiki". Use the `--help` option to list all available options.
+
+### Automation
+
+Now you want probably not run this command everytime yourself when it is
+needed. Therefore add a few auto commands to your vimrc. Here is a suggestion:
+
+    # Always save automatically a wiki entry when leaving buffer.
+    :au BufLeave *.wiki w
+
+    # Use command to create and update the recent file through a script.
+    :au BufCreate,BufEnter recent.wiki silent ! vimwiki_recent -d ~/vimwiki
+
+Now each time you visit a .wiki file in your vimwiki, its modification time
+will be changed when you leave the buffer. The other command will run the
+script to update the "recent.wiki" file each time just before you open the
+"recent.wiki" file. It's content lists all last edited or visited files in
+order as a typical wiki link.
+
+### Add the file to your overview
+
+The last thing you need to do is actually linking the file at
+"~/vimwiki/recent.wiki" as a link to your overview page at
+"~/vimwiki/index.wiki". And I suggest to add it at the top, so each time you
+open your index page, the most recent files will be available at your
+fingertips.
+
+At that point you don't need to do anything else ever again. The script should
+run automatically each time when needed and provide you the latest edited wiki
+entries.
---
+++ vimwiki_recent.py
@@ -0,0 +1,156 @@
+#!/bin/env python3
+
+import sys
+import os
+import pathlib
+import argparse
+
+
+def parse_arguments():
+
+    parser = argparse.ArgumentParser(
+        description=('Read your vimwiki files sorted by last used and create '
+                     'a list'),
+        epilog=('Copyright © 2022 Tuncay D. '
+                '<https://github.com/thingsiplay/vimwiki_recent>')
+    )
+
+    example = '~/vimwiki'
+    parser.add_argument(
+        '-d', '--dir',
+        metavar='DIR',
+        type=pathlib.Path,
+        required=True,
+        help=('root directory of your vimwiki, '
+              f'example: "{example}"'),
+    )
+
+    default = 'wiki'
+    parser.add_argument(
+        '-e', '--ext',
+        metavar='EXT',
+        default=default,
+        help=('file extension without dot of your vimwiki files, '
+              f'defaults to "{default}"'),
+    )
+
+    default = 'recent'
+    parser.add_argument(
+        '-r', '--recent',
+        metavar='NAME',
+        default=default,
+        help=('name of recent file to create, basename without extension, '
+              f'defaults to "{default}"'),
+    )
+
+    default = 'index,diary'
+    parser.add_argument(
+        '-i', '--ignore',
+        metavar='NAMES',
+        default=default,
+        help=('comma separated list of files and folders to ignore, only '
+              f'basename without extension, defaults to "{default}"'),
+    )
+
+    parser.add_argument(
+        '-X', '--dry-run',
+        default=False,
+        action='store_true',
+        help=('do not write recent file to filesystem')
+    )
+
+    parser.add_argument(
+        '-o', '--stdout',
+        default=False,
+        action='store_true',
+        help=('print recent content to stdout stream')
+    )
+
+    return parser.parse_args()
+
+
+def read_dir(directory, ext):
+    ext = "." + ext
+    list_of_files = []
+    for path, dirs, files in os.walk(directory):
+        for name in files:
+            file = pathlib.PurePath(path, name)
+            if file.suffix == ext:
+                list_of_files.append(file)
+    return list_of_files
+
+
+def sort_files(files):
+    files.sort(key=lambda f: os.path.getmtime(f))
+    files.reverse()
+    return files
+
+
+def remove_ignored_names(files, ignore_names):
+    return [file for file in files if file.stem not in ignore_names]
+
+
+def files_to_relative_dir(files, relative_dir):
+    return [file.relative_to(relative_dir) for file in files]
+
+
+def file_to_wiki(file):
+    link = "[[" + file.as_posix() + "|" + file.stem + "]]"
+    if file.parent.stem:
+        return link + " (" + file.parent.stem + ")"
+    else:
+        return link
+
+
+def convert_to_links(files):
+    return [file_to_wiki(file) for file in files]
+
+
+def write_recent_file(file, content):
+    file.write_text("\n".join(content))
+
+
+def write_stdout(content):
+    print("\n".join(content))
+
+
+def path_not_exist(path):
+    try:
+        if not path.exists():
+            raise FileNotFoundError(f'Path does not exist: "{path}"')
+    except FileNotFoundError as e:
+        print(e, file=sys.stderr)
+        return 1
+    return 0
+
+
+def main():
+
+    args = parse_arguments()
+
+    wiki_dir = pathlib.Path(args.dir).expanduser().resolve()
+    if path_not_exist(wiki_dir):
+        return 1
+
+    recent_file = f"{wiki_dir}/{args.recent}.{args.ext}"
+    recent_file = pathlib.Path(recent_file).resolve()
+    ignore = args.ignore.split(',') + [args.recent]
+
+    files = read_dir(wiki_dir, args.ext)
+    files = sort_files(files)
+    files = remove_ignored_names(files, ignore)
+
+    relative_files = files_to_relative_dir(files, wiki_dir)
+    wiki_links = convert_to_links(relative_files)
+
+    if not args.dry_run:
+        write_recent_file(recent_file, wiki_links)
+
+    if args.stdout:
+        write_stdout(wiki_links)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
